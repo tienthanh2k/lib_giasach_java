bc34b1ec06dcbf11d9b2b137ce469dc67d4e1a51
DESKTOP-O6N8CQF\Admin <thanhbncp@gmail.com>
2025-06-15 21:42:23 +0700

    feat: bo sung file bat

---
+++ setup-hooks.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Copying Git hooks...
+
+REM Tạo thư mục hooks nếu chưa có
+IF NOT EXIST ".git\hooks" (
+    echo Error: .git\hooks folder not found. Make sure you're inside a Git repo.
+    exit /b 1
+)
+
+copy hooks\commit-msg .git\hooks\commit-msg > nul
+
+echo Git hook installed successfully!
+pause
